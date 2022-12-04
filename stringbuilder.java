1ff573894fe06ab5a3bc0574aebe58905d2d5336
bhargav <102895435+bhargavsai259@users.noreply.github.com>
2022-12-04 13:11:10 +0530

    Create stringbuilder.java

---
+++ stringbuilder.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	  Scanner sc=new Scanner(System.in);
+	    
+	     StringBuffer s=new StringBuffer(sc.next());
+
+	   // s.insert(0,"begining"); // 2 args
+	   // s.append("ender");// 1 args
+        //s.replace(0,1,"over"); //3 args
+        //s.delete(0,3); //2 args
+        //s.reverse()
+        //substring can have one or two args
+        
+        
+        		
+	}
+}
